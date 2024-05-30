e22f90746c3c0ced7716bbcef6bce5f4812d9539
hojay <jeahonglee@gmail.com>
2024-05-31 00:17:49 +0900

    modal button function setup

--- screens/ManageExpense.js
+++ screens/ManageExpense.js
@@ -2,12 +2,23 @@ import { useLayoutEffect } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import IconButton from '../components/UI/IconButton';
 import { GlobalStyles } from '../constants/styles';
+import CustomButton from '../components/UI/CustomButton';
 
 function ManageExpense({ route, navigation }) {
   const editedExpenseId = route.params?.expenseId;
   const isEditing = !!editedExpenseId;
 
-  function deleteExpenseHandler() {}
+  function deleteExpenseHandler() {
+    navigation.goBack();
+  }
+
+  function cancelHandler() {
+    navigation.goBack();
+  }
+
+  function confirmHandler() {
+    navigation.goBack();
+  }
 
   useLayoutEffect(() => {
     navigation.setOptions({
@@ -17,6 +28,14 @@ function ManageExpense({ route, navigation }) {
 
   return (
     <View style={styles.container}>
+      <View style={styles.buttonContainer}>
+        <CustomButton style={styles.button} mode="flat" onPress={cancelHandler}>
+          Cancel
+        </CustomButton>
+        <CustomButton style={styles.button} onPress={confirmHandler}>
+          {isEditing ? 'Update' : 'Add'}
+        </CustomButton>
+      </View>
       {isEditing && (
         <View style={styles.deleteContainer}>
           <IconButton
@@ -46,4 +65,10 @@ const styles = StyleSheet.create({
     borderTopColor: GlobalStyles.colors.primary200,
     alignItems: 'center',
   },
+  buttonContainer: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  button: { minWidth: 120, marginHorizontal: 8 },
 });
